552a63a92cee6bd2840608927973b93f2b2d6a22
AkashDY <89009592+AkashDY@users.noreply.github.com>
2023-06-10 16:29:12 +0530

    added all files

---
+++ Storing and deleting movies into the database/component/MoviesList.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import Movie from "./Movie";
+import classes from "./MoviesList.module.css";
+
+const MovieList = (props) => {
+  return (
+    <ul className={classes["movies-list"]}>
+      {props.movies.map((movie) => (
+        <li key={movie.id}>
+          <Movie
+            key={movie.id}
+            title={movie.title}
+            releaseDate={movie.releaseDate}
+            openingText={movie.openingText}
+          />
+          <button onClick={() => props.onDeleteMovie(movie.id)}>
+            Delete Movie
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default MovieList;

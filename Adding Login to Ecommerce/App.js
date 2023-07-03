86ca36f728e869cd6fc085b827cf93e3dbf95a2f
AkashDY <89009592+AkashDY@users.noreply.github.com>
2023-07-03 14:12:42 +0530

    Add files via upload

---
+++ Adding Login to Ecommerce/App.js
@@ -0,0 +1,31 @@
+import React, { Fragment } from "react";
+import { Route, Routes } from "react-router-dom";
+import Store from "./components/pages/Store";
+import About from "./components/pages/About";
+import Home from "./components/pages/Home";
+import Footer from "./components/footer/Footer";
+import Contact from "./components/pages/Contact";
+import ProductPage from "./components/pages/ProductPage";
+import LoginPage from "./components/pages/LoginPage";
+
+
+function App() {
+
+  return (
+    <Fragment>
+
+        <Routes>
+          <Route exact path="/" Component={Store}/>
+          <Route path="/about" Component={About}/>
+          <Route path="/Login" Component={LoginPage} />
+          <Route path="/contact" Component={Contact}/>
+          <Route path="/home" Component={Home}/>
+          <Route path="/product/:id" Component={ProductPage} />
+        </Routes>
+        <Footer/>
+    </Fragment>
+
+  );
+}
+
+export default App;
